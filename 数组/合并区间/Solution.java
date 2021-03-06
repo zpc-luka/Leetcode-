ebb0329ec97125e58d71f515cfdc27eb023c3af7
zongpc <zongpc@126.com>
2021-03-06 14:17:04 +0800

    合并区间

---
+++ 数组/合并区间/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals.length == 0){
+            return new int[0][2];
+        }
+
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] interval1, int[] interval2){
+                return interval1[0] - interval2[0];
+            }
+        });     // 对起点排序
+
+        List<int[]> merged = new ArrayList<>();
+
+        for(int i = 0; i < intervals.length; i++){
+            int L = intervals[i][0], R = intervals[i][1];
+            if(merged.size() == 0 || merged.get(merged.size() - 1)[1] < L){
+                merged.add(new int[]{L, R});
+            }else{
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() -1 )[1], R);
+
+            }
+        }
+
+        return merged.toArray(new int[merged .size()][]);
+    }
+}
